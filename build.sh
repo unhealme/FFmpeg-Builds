ce0b356a96ceb92d9bad163ac92a62d081313a91
unhealme <o2tongpethuk@gmail.com>
2025-08-06 02:11:17 +0700

    set default ffmpeg repo to https://git.ffmpeg.org/ffmpeg.git

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ fi
 rm -rf ffbuild
 mkdir ffbuild
 
-FFMPEG_REPO="${FFMPEG_REPO:-https://github.com/FFmpeg/FFmpeg.git}"
+FFMPEG_REPO="${FFMPEG_REPO:-https://git.ffmpeg.org/ffmpeg.git}"
 FFMPEG_REPO="${FFMPEG_REPO_OVERRIDE:-$FFMPEG_REPO}"
 GIT_BRANCH="${GIT_BRANCH:-master}"
 GIT_BRANCH="${GIT_BRANCH_OVERRIDE:-$GIT_BRANCH}"
