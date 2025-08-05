0cab5be30f6a0377e261d00dfeb824f5b86972ea
unhealme <o2tongpethuk@gmail.com>
2025-08-06 00:56:24 +0700

    more addins

--- addins/lto.sh
+++ addins/lto.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-FF_CONFIGURE="$FF_CONFIGURE --enable-lto"
+# shellcheck disable=SC2016
+FF_CONFIGURE+=" --enable-lto"
 
 ffbuild_dockeraddin() {
-    to_df 'ENV CFLAGS="$CFLAGS -flto=auto" CXXFLAGS="$CXXFLAGS -flto=auto" LDFLAGS="$LDFLAGS -flto=auto"'
+    to_df 'ENV CFLAGS="${CFLAGS} -flto=auto"'
+    to_df 'ENV CXXFLAGS="${CXXFLAGS} -flto=auto"'
+    to_df 'ENV LDFLAGS="${LDFLAGS} -flto=auto"'
+    to_df 'ENV FFLAGS="${FFLAGS} -flto=auto"'
+    to_df 'ENV FCFLAGS="${FCFLAGS} -flto=auto"'
+    to_df 'ENV RUSTFLAGS="${RUSTFLAGS} -Clto"'
+    to_df 'ENV CARGO_PROFILE_RELEASE_LTO=fat'
     to_df 'ENV FFBUILD_TARGET_FLAGS="$FFBUILD_TARGET_FLAGS --ar=${FFBUILD_TOOLCHAIN}-gcc-ar --nm=${FFBUILD_TOOLCHAIN}-gcc-nm --ranlib=${FFBUILD_TOOLCHAIN}-gcc-ranlib"'
 }
