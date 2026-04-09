a7c515e2cf342072fd45acbed89c84b7eaa791e3
unhealme <o2tongpethuk@gmail.com>
2026-04-09 21:48:28 +0700

    build: set distinct version from main

--- build.sh
+++ build.sh
@@ -52,7 +52,7 @@ cat <<EOF >"$BUILD_SCRIPT"
         --extra-cflags="\$FF_CFLAGS" --extra-cxxflags="\$FF_CXXFLAGS" --extra-libs="\$FF_LIBS" \
         --extra-ldflags="\$FF_LDFLAGS" --extra-ldexeflags="\$FF_LDEXEFLAGS" \
         --cc="\$CC" --cxx="\$CXX" --ar="\$AR" --ranlib="\$RANLIB" --nm="\$NM" \
-        --extra-version="\$(date +%Y%m%d)"
+        --extra-version="\$(date +%Y%m%d)@yt-dlp"
     make -j\$(nproc) V=1
     make install install-doc
 EOF
